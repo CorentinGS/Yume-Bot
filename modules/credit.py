4cff443572c89bc0927cfd641dc0cada1f9d1ae4
yumenetwork <yumenetwork@protonmail.com>
2018-10-02 19:41:08 +0200

    About command

--- modules/credit.py
+++ modules/credit.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
+from modules.utils import checks
 import json
+from datetime import datetime
+import os
+import psutil
+
 
 class Credit:
 
@@ -9,6 +14,7 @@ class Credit:
     def __init__(self, client, config):
         self.client = client
         self.config = config
+        self.process = psutil.Process(os.getpid())
 
         global conf
         conf = config
@@ -18,14 +24,14 @@ class Credit:
     @commands.cooldown(1, 60, commands.BucketType.user)
     async def credit(self, ctx):
 
-        with open('./config.json', 'r') as cjson:
+        with open('./config/config.json', 'r') as cjson:
                 config = json.load(cjson)
 
         VERSION = config["version"]
         OWNER = config["owner_id"]
 
         message = ctx.message
-        message.delete()
+        await message.delete()
         owner = await self.client.get_user_info(OWNER)
 
         embed = discord.Embed(
@@ -49,7 +55,7 @@ class Credit:
     async def lib(self, ctx):
 
         message = ctx.message
-        message.delete()
+        await message.delete()
 
 
         embed = discord.Embed(
@@ -64,6 +70,29 @@ class Credit:
         embed.add_field(name="Discord Support", value="https://discordapp.com/invite/r3sSKJJ", inline=True)
 
         return await ctx.send(embed=embed)
+
+
+    @commands.command(pass_context = True)
+    @checks.is_owner()
+    async def about(self, ctx):
+
+        message = ctx.message
+        await message.delete()
+
+        ramUsage = self.process.memory_full_info().rss / 1024**2
+
+        embed = discord.Embed(
+            title="About",
+            color= ctx.me.top_role.colour
+        )
+
+        embed.add_field(name="RAM", value=f"{ramUsage:.2f} MB", inline=True)
+        embed.add_field(name="Servers", value=len(self.client.guilds), inline=True)
+
+
+        return await ctx.send(embed = embed)
+
+
 
 def setup(client):
     client.add_cog(Credit(client, client.config))
