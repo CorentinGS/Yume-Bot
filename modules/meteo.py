4d0b3672f39417b5d3828510ad0ec4a30087f1f4
yumenetwork <yumenetwork@protonmail.com>
2018-10-06 14:05:11 +0200

    Meteo modules

---
+++ modules/meteo.py
@@ -0,0 +1,84 @@
+import discord
+from discord.ext import commands
+import json
+import requests
+from .utils.weather import url_meteo, data_fetch, data_return
+
+
+class Meteo:
+
+    conf = {}
+
+    def __init__(self, bot, config):
+        self.bot = bot
+        self.config = config
+
+        global conf
+        conf = config
+
+    @commands.command(pass_context=True)
+    async def gmeteo(self, ctx, city: str = "Paris"):
+
+        msg = ctx.message
+        await msg.delete()
+
+        result = url_meteo(city)
+        fetch = data_fetch(result)
+        data = data_return(fetch)
+
+        embed = discord.Embed(
+            title="Meteo",
+            color=discord.Colour.dark_red()
+        )
+
+        embed.add_field(name='City', value=data['city'], inline=True)
+        embed.add_field(name='Country', value=data['country'], inline=True)
+        embed.add_field(name='Temperature max', value="{}°C".format(
+            data['temp_max']), inline=True)
+        embed.add_field(name='Temperature min', value='{}°C'.format(
+            data['temp_min']), inline=True)
+        embed.add_field(name='Humidity', value="{}%".format(
+            data['humidity']), inline=True)
+        embed.add_field(name='Pressure', value="{}hPa".format(
+            data['pressure']), inline=True)
+        embed.add_field(name='Conditions', value=data['sky'], inline=False)
+        embed.add_field(name='Sunrise', value=data['sunrise'], inline=True)
+        embed.add_field(name='Sunset', value=data['sunset'], inline=True)
+        embed.add_field(
+            name='Wind', value="{}m/s".format(data['wind']), inline=True)
+        embed.add_field(name='Cloudiness', value="{}%".format(
+            data['cloudiness']), inline=True)
+
+        return await ctx.send(embed=embed)
+
+    @commands.command(pass_context=True, aliases=["mto", "weather"])
+    async def meteo(self, ctx, city: str = "Paris"):
+
+        msg = ctx.message
+        await msg.delete()
+
+        result = url_meteo(city)
+        fetch = data_fetch(result)
+        data = data_return(fetch)
+
+        embed = discord.Embed(
+            title="Meteo",
+            color=discord.Colour.dark_red()
+        )
+
+        embed.add_field(name='City', value=data['city'], inline=True)
+        embed.add_field(name='Country', value=data['country'], inline=True)
+        embed.add_field(name='Temperature', value="{}°C".format(
+            data['temp']), inline=True)
+        embed.add_field(name='Conditions', value="{}".format(
+            data['description']), inline=False)
+        embed.add_field(
+            name='Wind', value="{}m/s".format(data['wind']), inline=True)
+        embed.add_field(name='Cloudiness', value="{}%".format(
+            data['cloudiness']), inline=True)
+
+        return await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Meteo(bot, bot.config))
---
+++ modules/utils/weather.py
@@ -0,0 +1,52 @@
+import json
+import urllib.request
+import datetime
+
+with open('./config/keys.json', 'r') as cjson:
+    keys = json.load(cjson)
+
+
+def time_converter(time):
+    converted_time = datetime.datetime.fromtimestamp(
+        int(time)
+    ).strftime('%I:%M %p')
+    return converted_time
+
+
+def url_meteo(city):
+    user_api = keys["weather"]
+    unit = 'metric'
+    api = 'http://api.openweathermap.org/data/2.5/weather?q='
+
+    full_url = api + str(city) + '&mode=json&units=' + \
+        unit + '&APPID=' + user_api
+    return full_url
+
+
+def data_fetch(full_url):
+    url = urllib.request.urlopen(full_url)
+    output = url.read().decode('utf-8')
+    raw_api_dict = json.loads(output)
+    url.close()
+    return raw_api_dict
+
+
+def data_return(raw_api_dict):
+    data = dict(
+        city=raw_api_dict.get('name'),
+        country=raw_api_dict.get('sys').get('country'),
+        temp=raw_api_dict.get('main').get('temp'),
+        temp_max=raw_api_dict.get('main').get('temp_max'),
+        temp_min=raw_api_dict.get('main').get('temp_min'),
+        humidity=raw_api_dict.get('main').get('humidity'),
+        pressure=raw_api_dict.get('main').get('pressure'),
+        sky=raw_api_dict['weather'][0]['main'],
+        description=raw_api_dict['weather'][0]['description'],
+        sunrise=time_converter(raw_api_dict.get('sys').get('sunrise')),
+        sunset=time_converter(raw_api_dict.get('sys').get('sunset')),
+        wind=raw_api_dict.get('wind').get('speed'),
+        wind_dir=raw_api_dict.get('wind').get('code'),
+        cloudiness=raw_api_dict.get('clouds').get('all')
+    )
+
+    return data
