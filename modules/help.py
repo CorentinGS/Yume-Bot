ab7d7dd56d5734558a0d5d3408f652edd3bb9a96
yumenetwork <yumenetwork@protonmail.com>
2018-10-11 21:19:27 +0200

    Help

--- modules/help.py
+++ modules/help.py
@@ -18,7 +18,7 @@ class Help:
 
     @commands.command(pass_context=True)
     @commands.guild_only()
-    async def help(self, ctx):
+    async def help(self, ctx, *, arg : str = None):
 
 
         with open('./config/config.json', 'r') as cjson:
@@ -27,26 +27,50 @@ class Help:
         prefix = config["prefix"]
 
         msg = ctx.message
+        await msg.delete()
         author = ctx.message.author
-        embed = discord.Embed(
-            set_author="Help Menu",
-            color=discord.Colour.orange()
-        )
 
-        embed.add_field(name = "**General**", value= f"{prefix}help General")
-        embed.add_field(name = "**Moderation**", value= f"{prefix}help Moderation")
-        embed.add_field(name = "**Fun**", value= f"{prefix}help Fun")
-        embed.add_field(name = "**Meteo**", value= f"{prefix}help Meteo")
-        embed.add_field(name = "**Blacklist**", value= f"{prefix}help Blacklist")
-        embed.add_field(name = "**Info**", value= f"{prefix}help Info")
+        if arg is None:
+            embed = discord.Embed(
+                set_author="Help Menu",
+                color=discord.Colour.orange()
+            )
+
+            embed.add_field(name = "**General**", value= f"{prefix}help General")
+            embed.add_field(name = "**Moderation**", value= f"{prefix}help Moderation")
+            embed.add_field(name = "**Fun**", value= f"{prefix}help Fun")
+            embed.add_field(name = "**Meteo**", value= f"{prefix}help Meteo")
+            embed.add_field(name = "**Blacklist**", value= f"{prefix}help Blacklist")
+            embed.add_field(name = "**Info**", value= f"{prefix}help Info")
 
 
-        try:
-            await msg.delete()
-            return await author.send(embed=embed)
+            try:
 
-        except discord.HTTPException:
-            pass
+                return await ctx.send(embed=embed)
+
+            except discord.HTTPException:
+                pass
+
+        elif arg == 'general':
+            embed = discord.Embed(
+                set_author='General Help',
+                color=discord.Colour.blue()
+            )
+
+            embed.add_field(name = "**Help**", value=f"{prefix}help")
+            embed.add_field(name = "**Info**", value=f"{prefix}info (Server Informations)" )
+            embed.add_field(name = "**Members**", value=f"{prefix}members (How many members ?)" )
+
+            try:
+                return await ctx.send(embed = embed)
+
+            except discord.HTTPException:
+                pass
+
+        else:
+            return await ctx.send("Invalid argument")
+
+
 
 
 def setup(bot):
