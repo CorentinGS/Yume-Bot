f467835b231ad750aede69339545d5092450067d
Zenrac <zenrac@outlook.fr>
2018-10-13 19:47:36 +0200

    Add files via upload

---
+++ modules/utils/db.py
@@ -0,0 +1,26 @@
+import motor.motor_asyncio
+
+
+class Settings():
+
+    def __init__(self):
+        self.client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
+        self.db = self.client.bot
+        self.glob = self.db.glob
+        self.guilds = self.db.guilds
+
+    async def get_glob_settings(self):
+        doc = await self.glob.find_one({"_id": 0})
+        if not doc:
+            return {"_id" : {}}
+        return doc.get("_id")
+
+    async def set_glob_settings(self, settings):
+        return await self.glob.replace_one({"_id": 0}, settings, True)
+
+    async def set_server_settings(self, id, settings):
+        return await self.guilds.replace_one({"_id": id}, settings, True)
+
+    async def get_server_settings(self, id):
+        doc = await self.guilds.find_one({"_id": id})
+        return doc or {}
