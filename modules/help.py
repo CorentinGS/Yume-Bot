076a513ca17162478a852c8305f2bc445a78df59
yumenetwork <yumenetwork@protonmail.com>
2018-10-25 09:46:24 +0200

    Test new Help

--- modules/help.py
+++ modules/help.py
@@ -14,6 +14,7 @@ class Help:
         global conf
         conf = config
 
+        '''
     @commands.command(pass_context=True)
     @commands.guild_only()
     async def help(self, ctx, *, arg: str = None):
@@ -204,6 +205,28 @@ class Help:
 
         else:
             return await ctx.send("Invalid argument")
+'''
+
+    @commands.command(pass_context=True)
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def help(self, ctx):
+
+        msg = ctx.message
+
+        await msg.delete()
+
+        embed = discord.Embed(
+            set_author='Help',
+            color=discord.Colour.magenta()
+        )
+
+        embed.add_field(
+            name="**Commands**", value="[Click here for a full commands list](https://yumenetwork.gitbook.io/yumebot/)\n", inline=False)
+        embed.add_field(
+            name="**Invite**", value="[Click Here](https://discordapp.com/api/oauth2/authorize?client_id=456504213262827524&permissions=8&redirect_uri=https%3A%2F%2Fgithub.com%2Fyumenetwork%2FYume-Bot&scope=bot) to invite the bot", inline=False)
+        embed.add_field(name= "**Support**", value="Join [YumeBot Support](https://invite.gg/yumenetwork) if you want to get help.", inline=False)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
