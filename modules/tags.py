78f036be5b4f151071da51e14f6d3db2631fe772
yumenetwork <yumenetwork@protonmail.com>
2018-10-31 08:53:36 +0100

    Add tag

---
+++ modules/tags.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import json
+import os
+import psutil
+
+with open('modules/utils/tag.json', 'r') as cjson:
+    tags = json.load(cjson)
+
+class Tags:
+
+    conf = {}
+
+    def __init__(self, bot, config):
+        self.bot = bot
+        self.config = config
+
+        global conf
+        conf = config
+
+
+
+    @commands.command()
+    async def tag(self, ctx, *, name: str=None):
+        await ctx.message.delete()
+
+        if name in tags:
+            await ctx.send(f"{tags[str(name)]}")
+        else:
+            await ctx.send("Unknown tag")
+
+
+def setup(bot):
+    bot.add_cog(Tags(bot, bot.config))
