a21f50812959bd82f88938e2284aff21f385b6db
yumenetwork <yumenetwork@protonmail.com>
2018-11-01 16:26:43 +0100

    patch

--- modules/utils/db.py
+++ modules/utils/db.py
@@ -5,11 +5,12 @@ class Settings():
 
     def __init__(self):
         self.client = motor.motor_asyncio.AsyncIOMotorClient('mongo', 27017)
+
         self.db = self.client.bot
+
         self.glob = self.db.glob
-        self.guilds = self.db.guilds
-        self.users = self.db.users
-        self.tag = self.db.tag
+        self.servers = self.db.servers
+
 
     async def get_glob_settings(self):
         doc = await self.glob.find_one({"_id": 0})
@@ -19,8 +20,8 @@ class Settings():
         return await self.glob.replace_one({"_id": 0}, settings, True)
 
     async def set_server_settings(self, id, settings):
-        return await self.guilds.replace_one({"_id": id}, settings, True)
+        return await self.servers.replace_one({"_id": id}, settings, True)
 
     async def get_server_settings(self, id):
-        doc = await self.guilds.find_one({"_id": id})
+        doc = await self.servers.find_one({"_id": id})
         return doc or {}
