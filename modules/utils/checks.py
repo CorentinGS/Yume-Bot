d05e29815b4853df2fb2b3585277fcbbff215074
yumenetwork <yumenetwork@protonmail.com>
2018-11-03 14:21:19 +0100

    Rewrite check & add dev

--- modules/utils/checks.py
+++ modules/utils/checks.py
@@ -1,24 +1,33 @@
 from discord.ext import commands
+import json
 
-owners = 282233191916634113
-
-
-def is_owner_check(ctx):
-    _id = ctx.message.author.id
-    return _id == owners
+with open('modules/utils/tag.json', 'r') as cjson:
+    check = json.load(cjson)
 
 
 def is_owner():
+    async def is_owner_check(ctx):
+        _id = ctx.message.author.id
+        if owner in check:
+            for owners in owner:
+                return _id == owners
+
     return commands.check(is_owner_check)
 
 
-def is_dm_check(ctx):
-    if ctx.guild is None:
-        return True
-
-
-def is_dm():
+def is_dm:
+    async def is_dm_check(ctx):
+        if ctx.guild is None:
+            return True
     return commands.check(is_dm_check)
+
+def is_dev:
+    async def is_dev_check(ctx):
+        _id = ctx.message.author.id
+        if dev in check:
+            for users in dev:
+                return _id == users
+    return commands.check(is_dev_check)
 
 
 def guild_only():
