1dfb64a28372b9ce3c029ffa4aed4c013bb8f72a
yumenetwork <yume.network@protonmail.com>
2018-11-24 15:13:31 +0100

    Add tips

--- modules/utils/lists.py
+++ modules/utils/lists.py
@@ -24,5 +24,5 @@ poney = [
 
 tip = [
     "Check our website : https://www.yumenetwork.fr", "Create your own profile using : --profile", "Share the bot to your friends",
-    "Don't forget to vote : https://discordbots.org/bot/456504213262827524"
+    "Don't forget to vote : https://discordbots.org/bot/456504213262827524", "Check the source here : https://anon.to/VNzGBX"
 ]
