3845dddc0aacbf255e744e01cfc19dc641506bb0
yumenetwork <yume.network@protonmail.com>
2018-11-25 12:11:14 +0100

    Add API

---
+++ api/db.py
@@ -0,0 +1,39 @@
+import pymongo
+
+class Settings():
+
+    def __init__(self):
+        self.client = pymongo.MongoClient('mongo', 27017)
+
+        self.db = self.client.bot
+
+        self.glob = self.db.glob
+        self.servers = self.db.servers
+        self.profiles = self.db.profiles
+        self.keys = self.db.keys
+
+
+    def get_glob_settings(self):
+        doc = self.glob.find_one({"_id": 0})
+        return doc or {}
+
+    def set_glob_settings(self, settings):
+        return self.glob.replace_one({"_id": 0}, settings, True)
+
+    def set_server_settings(self, id, settings):
+        return self.servers.replace_one({"_id": id}, settings, True)
+
+    def get_server_settings(self, id):
+        doc = self.servers.find_one({"_id": id})
+        return doc or {}
+
+    def get_user_settings(self, id):
+        doc =  self.profiles.find_one({"_id": id})
+        return doc or {}
+
+    def set_user_settings(self, id, settings):
+        return self.profiles.replace_one({"_id": id}, settings, True)
+
+    def get_key_settings(self, name):
+        doc = self.keys.find_one({"_id" : name})
+        return doc or {}
---
+++ api/rest.py
@@ -0,0 +1,76 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
+from flask_httpauth import HTTPBasicAuth
+
+
+from db import Settings
+
+app = Flask(__name__)
+api = Api(app)
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_password(username):
+    set = Settings().get_key_settings(str(username))
+    if not set:
+        return None
+    else:
+        return set[str('key')]
+
+
+@auth.error_handler
+def unauthorized():
+    return 'Not authorized', 404
+
+
+class User(Resource):
+    @auth.login_required
+    def get(self, id):
+
+        if not Settings().get_user_settings(str(id)):
+            return "User not found", 404
+        else:
+            set = Settings().get_user_settings(str(id))
+            return set, 200
+
+
+    def post(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('gender')
+        parser.add_argument('status')
+        parser.add_argument('lover')
+        args = parser.parse_args()
+
+        if Settings().get_user_settings(str(id)):
+            return "User already exist", 404
+        else:
+            set = Settings().get_user_settings(str(id))
+            set["gender"] = args['gender']
+            set['status'] = args['status']
+            set['lover'] = args['lover']
+
+            Settings().set_user_settings(str(id), set)
+
+        return set, 201
+
+    def put(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('gender')
+        parser.add_argument('status')
+        parser.add_argument('lover')
+        args = parser.parse_args()
+
+        set = Settings().get_user_settings(str(id))
+        set["gender"] = args['gender']
+        set['status'] = args['status']
+        set['lover'] = args['lover']
+
+        Settings().set_user_settings(str(id), set)
+
+        return set, 201
+
+
+api.add_resource(User, "/user/<string:id>")
+
+app.run(host = '0.0.0.0', debug=True)
