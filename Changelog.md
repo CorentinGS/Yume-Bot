3b1c065d99a4dc16dad69cdf1b07ab9c52ce1886
yumenetwork <yume.network@protonmail.com>
2018-11-30 13:32:14 +0100

    Up

--- Changelog.md
+++ Changelog.md
@@ -4,7 +4,15 @@ All notable changes to this project will be documented in this file. The format 
 [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to
 [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.4.8](https://github.com/yumepantai/Yume-Bot/releases/tag/1.4.7) - 2018-11-30
 
+### Added
+
+- Get settings
+
+
+
+* * *
 
 ## [1.4.7](https://github.com/yumepantai/Yume-Bot/releases/tag/1.4.7) - 2018-11-30
 
--- modules/settings.py
+++ modules/settings.py
@@ -23,7 +23,6 @@ class Set:
         if ctx.invoked_subcommand is None:
             await ctx.invoke(self.get)
 
-
     @setting.command()
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
@@ -33,7 +32,6 @@ class Set:
 
         def check(reaction, user):
             return user == ctx.message.author and str(reaction.emoji)
-
 
         if not 'mute' in set:
             set['muteRole'] = False
@@ -86,201 +84,201 @@ class Set:
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
     async def edit(self, ctx):
-            vip = False
+        vip = False
 
-            guild = ctx.message.guild
-            set = await Settings().get_server_settings(str(guild.id))
-            em = await Embeds().format_set_embed(ctx, guild, 'setting', vip)
-            glob = await Settings().get_glob_settings()
+        guild = ctx.message.guild
+        set = await Settings().get_server_settings(str(guild.id))
+        em = await Embeds().format_set_embed(ctx, guild, 'setting', vip)
+        glob = await Settings().get_glob_settings()
 
-            def check(reaction, user):
-                return user == ctx.message.author and str(reaction.emoji)
+        def check(reaction, user):
+            return user == ctx.message.author and str(reaction.emoji)
 
-            def msgcheck(m):
-                if m.author == ctx.message.author:
-                    return True
-                else:
-                    return False
-
-            msg = await ctx.send(embed=em)
-            reactions = ['🇲', '🇬', '⛔', '🖊', '🔨', '❌']
-            for reaction in reactions:
-                await msg.add_reaction(reaction)
-
-            try:
-                reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
-
-            except asyncio.TimeoutError:
-                await ctx.send('👎')
-
+        def msgcheck(m):
+            if m.author == ctx.message.author:
+                return True
             else:
-                if reaction.emoji == '🇲':
-                    await msg.clear_reactions()
-                    em = await Embeds().format_set_embed(ctx, guild, 'mutemenu', vip)
-                    await msg.edit(embed=em)
-                    reactions = ['💂', '💣', '❌']
-                    for reaction in reactions:
-                        await msg.add_reaction(reaction)
-                    try:
-                        reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
+                return False
 
-                    except asyncio.TimeoutError:
-                        await ctx.send('👎', delete_after=10)
+        msg = await ctx.send(embed=em)
+        reactions = ['🇲', '🇬', '⛔', '🖊', '🔨', '❌']
+        for reaction in reactions:
+            await msg.add_reaction(reaction)
 
-                    else:
-                        if reaction.emoji == '💂':
-                            arg = "on"
-                            await ctx.invoke(self.muterole, arg)
-                            await msg.delete()
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '💣':
-                            arg = "off"
-                            await ctx.invoke(self.muterole, arg)
-                            await msg.delete()
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '❌':
-                            await msg.delete()
-                            return
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
 
-                elif reaction.emoji == '🔨':
-                    await msg.clear_reactions()
-                    if ctx.message.author in glob["VIP"]:
-                        vip = True
-                    em = await Embeds().format_set_embed(ctx, guild, 'automenu', vip)
-                    await msg.edit(embed=em)
-                    reactions = ['✅', '🚫', '❌']
-                    if vip is True:
-                        reactions.extend(['⛔'])
-                    for reaction in reactions:
-                        await msg.add_reaction(reaction)
+        except asyncio.TimeoutError:
+            await ctx.send('👎')
 
-                    try:
-                        reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
+        else:
+            if reaction.emoji == '🇲':
+                await msg.clear_reactions()
+                em = await Embeds().format_set_embed(ctx, guild, 'mutemenu', vip)
+                await msg.edit(embed=em)
+                reactions = ['💂', '💣', '❌']
+                for reaction in reactions:
+                    await msg.add_reaction(reaction)
+                try:
+                    reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
 
-                    except asyncio.TimeoutError:
-                        await ctx.send('👎', delete_after=10)
+                except asyncio.TimeoutError:
+                    await ctx.send('👎', delete_after=10)
 
-                    else:
-                        if reaction.emoji == '✅':
-                            set["automod"] = True
-                            await msg.delete()
-                            await Settings().set_server_settings(str(guild.id), set)
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '🚫':
-                            set['automod'] = False
-                            await msg.delete()
-                            await Settings().set_server_settings(str(guild.id), set)
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '⛔':
-                            await msg.delete()
-                            await ctx.send("Not ready ! Ccoming Soon")
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '❌':
-                            await msg.delete()
-                            return
+                else:
+                    if reaction.emoji == '💂':
+                        arg = "on"
+                        await ctx.invoke(self.muterole, arg)
+                        await msg.delete()
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '💣':
+                        arg = "off"
+                        await ctx.invoke(self.muterole, arg)
+                        await msg.delete()
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '❌':
+                        await msg.delete()
+                        return
 
-                elif reaction.emoji == '🖊':
-                    if ctx.message.author in glob["VIP"]:
-                        vip = True
+            elif reaction.emoji == '🔨':
+                await msg.clear_reactions()
+                if ctx.message.author in glob["VIP"]:
+                    vip = True
+                em = await Embeds().format_set_embed(ctx, guild, 'automenu', vip)
+                await msg.edit(embed=em)
+                reactions = ['✅', '🚫', '❌']
+                if vip is True:
+                    reactions.extend(['⛔'])
+                for reaction in reactions:
+                    await msg.add_reaction(reaction)
 
-                    await msg.clear_reactions()
-                    em = await Embeds().format_set_embed(ctx, guild, 'loggingmenu', vip)
-                    await msg.edit(embed=em)
-                    reactions = ['📋', '🆓', '❔', '❌']
-                    '''
+                try:
+                    reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
+
+                except asyncio.TimeoutError:
+                    await ctx.send('👎', delete_after=10)
+
+                else:
+                    if reaction.emoji == '✅':
+                        set["automod"] = True
+                        await msg.delete()
+                        await Settings().set_server_settings(str(guild.id), set)
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '🚫':
+                        set['automod'] = False
+                        await msg.delete()
+                        await Settings().set_server_settings(str(guild.id), set)
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '⛔':
+                        await msg.delete()
+                        await ctx.send("Not ready ! Ccoming Soon")
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '❌':
+                        await msg.delete()
+                        return
+
+            elif reaction.emoji == '🖊':
+                if ctx.message.author in glob["VIP"]:
+                    vip = True
+
+                await msg.clear_reactions()
+                em = await Embeds().format_set_embed(ctx, guild, 'loggingmenu', vip)
+                await msg.edit(embed=em)
+                reactions = ['📋', '🆓', '❔', '❌']
+                '''
                     if vip is True:
                         reactions.extend(['🎬'])
                     '''
-                    for reaction in reactions:
-                        await msg.add_reaction(reaction)
-                    try:
-                        reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=120)
+                for reaction in reactions:
+                    await msg.add_reaction(reaction)
+                try:
+                    reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=120)
 
-                    except asyncio.TimeoutError:
-                        await ctx.send('👎')
+                except asyncio.TimeoutError:
+                    await ctx.send('👎')
 
-                    else:
-                        if reaction.emoji == '📋':
-                            arg = "on"
-                            await ctx.invoke(self.logging, arg)
-                            await msg.delete()
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '🆓':
-                            arg = 'off'
-                            await ctx.invoke(self.logging, arg)
-                            await msg.delete()
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '❔':
-                            await msg.delete()
-                            await ctx.send('Please name a Channel !')
-                            m = await self.bot.wait_for('message', timeout=60.0, check=msgcheck)
-                            text_channel = m.channel_mentions[0]
-                            await ctx.invoke(self.loggingchannel, text_channel)
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '❌':
-                            await msg.delete()
-                            return
-                elif reaction.emoji == '⛔':
-                    await msg.clear_reactions()
-                    em = await Embeds().format_set_embed(ctx, guild, 'blacklistmenu', vip)
-                    await msg.edit(embed=em)
-                    reactions = ['🚫', '🔓', '❌']
-                    for reaction in reactions:
-                        await msg.add_reaction(reaction)
-                    try:
-                        reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
+                else:
+                    if reaction.emoji == '📋':
+                        arg = "on"
+                        await ctx.invoke(self.logging, arg)
+                        await msg.delete()
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '🆓':
+                        arg = 'off'
+                        await ctx.invoke(self.logging, arg)
+                        await msg.delete()
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '❔':
+                        await msg.delete()
+                        await ctx.send('Please name a Channel !')
+                        m = await self.bot.wait_for('message', timeout=60.0, check=msgcheck)
+                        text_channel = m.channel_mentions[0]
+                        await ctx.invoke(self.loggingchannel, text_channel)
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '❌':
+                        await msg.delete()
+                        return
+            elif reaction.emoji == '⛔':
+                await msg.clear_reactions()
+                em = await Embeds().format_set_embed(ctx, guild, 'blacklistmenu', vip)
+                await msg.edit(embed=em)
+                reactions = ['🚫', '🔓', '❌']
+                for reaction in reactions:
+                    await msg.add_reaction(reaction)
+                try:
+                    reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
 
-                    except asyncio.TimeoutError:
-                        await ctx.send('👎')
+                except asyncio.TimeoutError:
+                    await ctx.send('👎')
 
-                    else:
-                        if reaction.emoji == '🚫':
-                            arg = True
-                            await ctx.invoke(self.bl, arg)
-                            await msg.delete()
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '🔓':
-                            arg = False
-                            await ctx.invoke(self.bl, arg)
-                            await msg.delete()
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '❌':
-                            await msg.delete()
-                            return
+                else:
+                    if reaction.emoji == '🚫':
+                        arg = True
+                        await ctx.invoke(self.bl, arg)
+                        await msg.delete()
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '🔓':
+                        arg = False
+                        await ctx.invoke(self.bl, arg)
+                        await msg.delete()
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '❌':
+                        await msg.delete()
+                        return
 
-                elif reaction.emoji == '🇬':
-                    await msg.clear_reactions()
-                    em = await Embeds().format_set_embed(ctx, guild, 'greetmenu', vip)
-                    await msg.edit(embed=em)
-                    reactions = ['❔', '📜', '❌']
-                    for reaction in reactions:
-                        await msg.add_reaction(reaction)
-                    try:
-                        reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
+            elif reaction.emoji == '🇬':
+                await msg.clear_reactions()
+                em = await Embeds().format_set_embed(ctx, guild, 'greetmenu', vip)
+                await msg.edit(embed=em)
+                reactions = ['❔', '📜', '❌']
+                for reaction in reactions:
+                    await msg.add_reaction(reaction)
+                try:
+                    reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60)
 
-                    except asyncio.TimeoutError:
-                        await ctx.send('👎')
+                except asyncio.TimeoutError:
+                    await ctx.send('👎')
 
-                    else:
-                        if reaction.emoji == '❔':
-                            await msg.delete()
-                            await ctx.send('Please name a Channel !')
-                            m = await self.bot.wait_for('message', timeout=60.0, check=msgcheck)
-                            text_channel = m.channel_mentions[0]
-                            await ctx.invoke(self.greetchannel, text_channel)
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '📜':
-                            arg = "on"
-                            await ctx.invoke(self.greet, arg)
-                            await msg.delete()
-                            await ctx.invoke(self.setting)
-                        elif reaction.emoji == '❌':
-                            await msg.delete()
-                            return
+                else:
+                    if reaction.emoji == '❔':
+                        await msg.delete()
+                        await ctx.send('Please name a Channel !')
+                        m = await self.bot.wait_for('message', timeout=60.0, check=msgcheck)
+                        text_channel = m.channel_mentions[0]
+                        await ctx.invoke(self.greetchannel, text_channel)
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '📜':
+                        arg = "on"
+                        await ctx.invoke(self.greet, arg)
+                        await msg.delete()
+                        await ctx.invoke(self.setting)
+                    elif reaction.emoji == '❌':
+                        await msg.delete()
+                        return
 
-                elif reaction.emoji == '❌':
-                    await msg.delete()
-                    return
+            elif reaction.emoji == '❌':
+                await msg.delete()
+                return
 
     @setting.command()
     @commands.guild_only()
@@ -377,7 +375,6 @@ class Set:
         await Settings().set_server_settings(server, set)
 
         await ctx.send('OK !', delete_after=5)
-
 
 
 def setup(bot):
