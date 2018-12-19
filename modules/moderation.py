8fc7c493c9aef80c04157bf7d1834cbb8a7e109b
yumenetwork <yume.network@protonmail.com>
2018-12-19 21:10:11 +0100

    Add mentions command into moderation

--- modules/moderation.py
+++ modules/moderation.py
@@ -346,6 +346,33 @@ class Moderation:
         else:
             await ctx.send(f'{len(members)} users were banned')
 
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def mention(self, ctx, role: str):
+
+        await ctx.message.delete()
+        rolemention = discord.utils.get(ctx.guild.roles, name=role)
+
+        if not rolemention.mentionable:
+            await rolemention.edit(mentionable=True)
+
+        await ctx.send(rolemention.mention)
+        await rolemention.edit(mentionable=False)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def annonce(self, ctx, role: str, *, content):
+        await ctx.message.delete()
+        rolemention = discord.utils.get(ctx.guild.roles, name=role)
+
+        if not rolemention.mentionable:
+            await rolemention.edit(mentionable=True)
+
+        await ctx.send("{} \n{}".format(rolemention.mention, content))
+        await rolemention.edit(mentionable=False)
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
