789efb5211734b714201d53b6e8542953a0d053d
yumenetwork <yume.network@protonmail.com>
2018-12-25 12:09:39 +0100

    Fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6.7-alpine
+FROM python:3.6.7-alpine3.8
 
 WORKDIR /usr/src/app
 
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ bot.config = config
 bot.ready = False
 
 log = logging.getLogger('bot')
-logging.basicConfig(level=logging.WARNING, filename="error.log", filemode="a+",
+logging.basicConfig(level=logging.ERROR, filename="error.log", filemode="a+",
                     format="%(asctime)-15s %(levelname)-8s %(message)s")
 
 print('Connecting...')
@@ -52,6 +52,7 @@ async def on_connect():
     print("Connected")
 
 
+
 @bot.event
 async def on_command_error(ctx, exception):
     log.error(str(exception))
@@ -59,10 +60,11 @@ async def on_command_error(ctx, exception):
         await ctx.send(str(exception))
 
 
+'''
 @bot.event
 async def on_error(event, *args, **kwargs):
-    log.error(event + ' : ' + str(args) + " " + str(kwargs))
-
+   cl log.error(event + ' : ' + str(args) + " " + str(kwargs))
+'''
 
 @bot.event
 async def on_ready():
