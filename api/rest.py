1dce5346bf87421c9329958f82eedf0f3572a2c2
Yumenetwork <yume.network@protonmail.com>
2019-06-26 19:45:30 +0200

    Major Fix
    Signed-off-by: yumenetwork <yume.network@protonmail.com>

--- api/rest.py
+++ api/rest.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-from db import Settings
-from flask import Flask
-from flask_httpauth import HTTPBasicAuth
-from flask_restful import Api, Resource, reqparse
-=======
+
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 from flask_restful import Api, Resource, reqparse
 
 import api.db as db
->>>>>>> lg
 
 app = Flask(__name__)
 api = Api(app)
@@ -30,10 +24,10 @@ def unauthorized():
 
 
 class Global(Resource):
-<<<<<<< HEAD
+
     @auth.login_required
     def get(self, id):
-        set = Settings().get_glob_settings
+        set = db().get_glob_settings
         return set, 200
 
     @staticmethod
@@ -41,40 +35,19 @@ class Global(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('VIP')
         args = parser.parse_args()
-        set = Settings().get_glob_settings
+        set = db().get_glob_settings
         set["VIP"] = args['VIP']
-        Settings().set_glob_settings(set)
-=======
-	@auth.login_required
-	def get(self, id):
-		set = db.Settings().get_glob_settings()
-		return set, 200
-
-	def put(self, id):
-		parser = reqparse.RequestParser()
-		parser.add_argument('VIP')
-
-		args = parser.parse_args()
-
-		set = db.Settings().get_glob_settings()
-		set["VIP"] = args['VIP']
-
-		db.Settings().set_glob_settings(set)
-
-		return set, 201
->>>>>>> lg
-
-
+        db().set_glob_settings(set)
 
 class Guild(Resource):
-<<<<<<< HEAD
+
     @auth.login_required
     def get(self, id):
 
-        if not Settings().get_server_settings(str(id)):
+        if not db().get_server_settings(str(id)):
             return 'Guild not found', 404
         else:
-            set = Settings().get_server_settings(str(id))
+            set = db().get_server_settings(str(id))
             return set, 200
 
     @staticmethod
@@ -91,7 +64,7 @@ class Guild(Resource):
 
         args = parser.parse_args()
 
-        set = Settings().get_user_settings(str(id))
+        set = db().get_user_settings(str(id))
         set["Greet"] = args['Greet']
         set['bl'] = args['bl']
         set['logging'] = args['logging']
@@ -100,49 +73,9 @@ class Guild(Resource):
         set['Admins'] = args["Admins"]
         set['Mods'] = args['Mods']
         set['Setup'] = args['Setup']
-        Settings().set_server_settings(str(id), set)
+        db().set_server_settings(str(id), set)
 
         return set, 201
-=======
-	@auth.login_required
-	def get(self, id):
-
-		if not db.Settings().get_server_settings(str(id)):
-			return 'Guild not found', 404
-		else:
-			set = db.Settings().get_server_settings(str(id))
-			return set, 200
-
-	def put(self, id):
-		parser = reqparse.RequestParser()
-		parser.add_argument('Greet')
-		parser.add_argument('bl')
-		parser.add_argument('logging')
-		parser.add_argument('GreetChannel')
-		parser.add_argument('LogChannel')
-		parser.add_argument('automod')
-		parser.add_argument('Setup')
-		parser.add_argument('Mods')
-		parser.add_argument('Admins')
-
-		args = parser.parse_args()
-
-		set = db.Settings().get_user_settings(str(id))
-		set["Greet"] = args['Greet']
-		set['bl'] = args['bl']
-		set['logging'] = args['logging']
-		set['GreetChannel'] = args['GreetChannel']
-		set['LogChannel'] = args['LogChannel']
-		set['automod'] = args['automod']
-		set['Admins'] = args["Admins"]
-		set['Mods'] = args['Mods']
-		set['Setup'] = args['Setup']
-		db.Settings().set_server_settings(str(id), set)
-
-		return set, 201
-
->>>>>>> lg
-
 
 api.add_resource(Guild, "/guild/<string:id>")
 api.add_resource(Global, "/global")
