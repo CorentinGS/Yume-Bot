b57a4ee530ca389c7f28b7d2951099ef099aeb9e
yumenetwork <yume.network@protonmail.com>
2019-09-22 15:58:21 +0200

    Add DBL support

--- modules/dbl.py
+++ modules/dbl.py
@@ -13,10 +13,10 @@ class DiscordBotsOrgAPI(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.token = token['dbl']
-        self.dblpy = dbl.Client(self.bot, self.token)
+        self.dblpy = dbl.DBLClient(self.bot, self.token)
         self.updating = self.bot.loop.create_task(self.update_stats())
 
-        self.dblpy = dbl.Client(self.bot, self.token)
+        self.dblpy = dbl.DBLClient(self.bot, self.token)
         self.updating = self.bot.loop.create_task(self.update_stats())
 
     async def update_stats(self):
