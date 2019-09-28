90518483f16744dc0767f08666a5736b6cd6c324
yumenetwork <yume.network@protonmail.com>
2019-09-28 14:06:31 +0200

    Add logging

--- modules/dbl.py
+++ modules/dbl.py
@@ -1,6 +1,5 @@
 import asyncio
 import json
-import logging
 
 import dbl
 from discord.ext import commands, tasks
@@ -23,26 +22,22 @@ class DiscordBotsOrgAPI(commands.Cog):
 
     @tasks.loop(minutes=30.0)
     async def update_stats(self):
-        logger.info('Attempting to post server count')
         try:
             await self.dblpy.post_guild_count()
-            logger.info('Posted server count ({})'.format(self.dblpy.guild_count()))
         except Exception as e:
-            logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            print('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
         await asyncio.sleep(1800)
-
 
     @commands.Cog.listener()
     async def on_dbl_vote(self, data):
-        logger.info('Received an upvote')
+        print('Received an upvote')
         user = await self.bot.fetch_user(data["user"])
-        server = self.get_guild(int(self.guild))
+        server = self.bot.get_guild(int(self.guild))
         for chan in server.channels:
             if chan.id == int(self.debug):
                 channel = chan
         await channel.send(f"{user} has voted")
 
+
 def setup(bot):
-    global logger
-    logger = logging.getLogger('bot')
     bot.add_cog(DiscordBotsOrgAPI(bot))
