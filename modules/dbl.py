5b52aca1d76ae8b661dd893f5a535b77f17680af
yumenetwork <yume.network@protonmail.com>
2019-09-28 11:14:48 +0200

    Update

--- modules/dbl.py
+++ modules/dbl.py
@@ -14,7 +14,6 @@ class DiscordBotsOrgAPI(commands.Cog):
         self.bot = bot
         self.token = token['dbl']
         self.dblpy = dbl.DBLClient(self.bot, self.token)
-        self.updating = self.bot.loop.create_task(self.update_stats())
 
     @tasks.loop(minutes=30.0)
     async def update_stats(self):
