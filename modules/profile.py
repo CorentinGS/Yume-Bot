e7a63cb8e6f5ae9e63db13e49505ef9cb3e8d4a3
yumenetwork <yume.network@protonmail.com>
2019-10-16 20:05:11 +0200

    Marriage in coming

--- modules/profile.py
+++ modules/profile.py
@@ -19,11 +19,27 @@ class Profile:
         # Settings
         self.vip: bool = False
 
+        # Marriage
+        self.married: bool = False
+        self.lover: int = 0
+
+
 
 class Profiles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
+    @commands.group()
+    @commands.guild_only()
+    async def marriage(self, ctx):
+        if ctx.invoked_subcommand is None:
+            return
+
+    @marriage.command()
+    @commands.guild_only()
+    async def ask(self, ctx):
+        return
 
 def setup(bot):
     bot.add_cog(Profiles(bot))
