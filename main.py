40eed223e3f8adff1e50130d5e673c13edb71fe6
yumenetwork <yume.network@protonmail.com>
2019-12-08 12:33:19 +0100

    Up

--- main.py
+++ main.py
@@ -40,20 +40,20 @@ import traceback
 import discord
 from discord.ext import commands
 
-with open('./config/config.json', 'r') as cjson:
+with open("./config/config.json", "r") as cjson:
     config = json.load(cjson)
 
-with open('./config/token.json', 'r') as cjson:
+with open("./config/token.json", "r") as cjson:
     token = json.load(cjson)
 
 modules = config["modules"]
 
 
 def get_prefix(bot, message):
-    prefixes = ['--', "y!", "yume", "yum", 'yume ', "yum ", "yume!"]
+    prefixes = ["--", "y!", "yume", "yum", "yume ", "yum ", "yume!"]
 
     if not message.guild:
-        return '?'
+        return "?"
 
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
@@ -66,55 +66,61 @@ logger.setLevel(logging.ERROR)
 
 class YumeBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=get_prefix, description=description,
-                         activity=discord.Game(name="Commands: --help"),
-                         pm_help=None, help_attrs=dict(hidden=True), fetch_offline_members=False)
+        super().__init__(
+            command_prefix=get_prefix,
+            description=description,
+            activity=discord.Game(name="Commands: --help"),
+            pm_help=None,
+            help_attrs=dict(hidden=True),
+            fetch_offline_members=False,
+        )
         self.uptime = datetime.datetime.utcnow()
-        self.token = token['token']
+        self.token = token["token"]
         self.ready = False
         self.config = config
         self.owner = config["owner_id"]
-        self.guild = config['support']
-        self.debug = config['debug']
+        self.guild = config["support"]
+        self.debug = config["debug"]
         self.remove_command("help")
 
     async def on_ready(self):
         if not self.ready:
             self.ready = True
-            print('Logged in.')
+            print("Logged in.")
             loaded = len(modules)
             for module in modules:
                 try:
-                    self.load_extension('modules.' + module)
+                    self.load_extension("modules." + module)
                 except Exception as e:
                     loaded -= 1
-                    print('Failed to load module {} : {}'.format(module, e))
+                    print("Failed to load module {} : {}".format(module, e))
                     traceback.print_exc()
-            print('{}/{} modules loaded'.format(loaded, len(modules)))
+            print("{}/{} modules loaded".format(loaded, len(modules)))
 
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
-            await ctx.author.send('This command cannot be used in private messages.')
+            await ctx.author.send("This command cannot be used in private messages.")
         elif isinstance(error, commands.DisabledCommand):
-            await ctx.author.send('Sorry. This command is disabled and cannot be used.')
+            await ctx.author.send("Sorry. This command is disabled and cannot be used.")
         elif isinstance(error, commands.MissingRequiredArgument):
-            return await ctx.send(f'Missing an argument.\n`{error}`')
+            return await ctx.send(f"Missing an argument.\n`{error}`")
         elif isinstance(error, commands.CheckFailure):
-            return await ctx.send(f'Missing a permission.\n`{error}`')
+            return await ctx.send(f"Missing a permission.\n`{error}`")
         elif isinstance(error, commands.CommandInvokeError):
             original = error.original
             if not isinstance(original, discord.HTTPException):
-                print(f'In {ctx.command.qualified_name}:', file=sys.stderr)
+                print(f"In {ctx.command.qualified_name}:", file=sys.stderr)
                 traceback.print_tb(original.__traceback__)
-                print(f'{original.__class__.__name__}: {original}', file=sys.stderr)
+                print(f"{original.__class__.__name__}: {original}", file=sys.stderr)
             elif isinstance(original, discord.Forbidden):
                 try:
-                    await ctx.send(f"I'm missing permissions to be able to do that.\n`{original}`")
+                    await ctx.send(
+                        f"I'm missing permissions to be able to do that.\n`{original}`"
+                    )
                 except discord.Forbidden:
                     return
         elif isinstance(error, commands.ArgumentParsingError):
             await ctx.send(error)
-
 
     async def close(self):
         await super().close()
@@ -122,12 +128,12 @@ class YumeBot(commands.Bot):
     async def on_guild_join(self, guild):
         await self.wait_until_ready()
         embed = discord.Embed(colour=discord.Color.green())
-        embed.title = 'New Guild'
-        embed.set_author(name='{0} <{0.id}>'.format(
-            guild.owner), icon_url=guild.owner.avatar_url)
-        embed.add_field(name='Server', value='{0.name} <{0.id}>'.format(guild))
-        embed.add_field(
-            name='Members', value='**{0}**'.format(len(guild.members)))
+        embed.title = "New Guild"
+        embed.set_author(
+            name="{0} <{0.id}>".format(guild.owner), icon_url=guild.owner.avatar_url
+        )
+        embed.add_field(name="Server", value="{0.name} <{0.id}>".format(guild))
+        embed.add_field(name="Members", value="**{0}**".format(len(guild.members)))
         embed.timestamp = datetime.datetime.now()
         server = self.get_guild(int(self.guild))
         for chan in server.channels:
@@ -138,12 +144,12 @@ class YumeBot(commands.Bot):
     async def on_guild_remove(self, guild):
         await self.wait_until_ready()
         embed = discord.Embed(colour=discord.Color.red())
-        embed.title = 'Left Guild'
-        embed.set_author(name='{0} <{0.id}>'.format(
-            guild.owner), icon_url=guild.owner.avatar_url)
-        embed.add_field(name='Server', value='{0.name} <{0.id}>'.format(guild))
-        embed.add_field(
-            name='Members', value='**{0}**'.format(len(guild.members)))
+        embed.title = "Left Guild"
+        embed.set_author(
+            name="{0} <{0.id}>".format(guild.owner), icon_url=guild.owner.avatar_url
+        )
+        embed.add_field(name="Server", value="{0.name} <{0.id}>".format(guild))
+        embed.add_field(name="Members", value="**{0}**".format(len(guild.members)))
         embed.timestamp = datetime.datetime.now()
         server = self.get_guild(int(self.guild))
         for chan in server.channels:
