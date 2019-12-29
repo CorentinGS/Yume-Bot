159b343949a641833793838ffa6fa656a5454605
yumenetwork <yume.network@protonmail.com>
2019-12-29 17:02:07 +0100

    Big rewrite stuff
    Signed-off-by: yumenetwork <yume.network@protonmail.com>

--- modules/sql/guilddb.py
+++ modules/sql/guilddb.py
@@ -51,6 +51,16 @@
 #  furnished to do so, subject to the following conditions:
 #
 #
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#
 import psycopg2
 from psycopg2 import extras
 
@@ -58,7 +68,6 @@ from modules.sql.guild import Guild
 
 try:
     con = psycopg2.connect("host=localhost dbname=yumebot user=postgres")
-    # cur = con.cursor()
     cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
 except psycopg2.DatabaseError as e:
     print('Error %s' % e)
