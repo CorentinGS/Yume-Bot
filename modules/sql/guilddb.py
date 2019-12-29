2f603462933475d008332079d971e4636677d9a1
yumenetwork <yume.network@protonmail.com>
2019-12-29 17:01:06 +0100

    Update for rewrite

--- modules/sql/guilddb.py
+++ modules/sql/guilddb.py
@@ -31,6 +31,26 @@
 #  furnished to do so, subject to the following conditions:
 #
 #
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#
 import psycopg2
 from psycopg2 import extras
 
@@ -130,4 +150,6 @@ class GuildDB:
     """
     Create & delete methods
     """
-   
+
+
+
