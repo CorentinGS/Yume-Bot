6dd0a7132b0950c5f58a4567dc203d046792151c
yumenetwork <yume.network@protonmail.com>
2020-01-01 10:44:57 +0100

    Add RPS
    Signed-off-by: yumenetwork <yume.network@protonmail.com>

--- modules/tasks.py
+++ modules/tasks.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2019.
+#  Copyright (c) 2020.
 #  MIT License
 #
 #  Copyright (c) 2019 YumeNetwork
@@ -31,10 +31,20 @@
 #  furnished to do so, subject to the following conditions:
 #
 #
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#
 from itertools import cycle
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import tasks, commands
 
 
 class Tasks(commands.Cog):
@@ -43,20 +53,15 @@ class Tasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = bot.config
+        self.chng_pr.start()
 
-    # self.chng_pr.start()
-
-    @tasks.loop(seconds=30.0)
+    @tasks.loop(seconds=10.0)
     async def chng_pr(self):
         status = ['--help', 'Peace and Dream', 'By YumeNetwork']
         status = cycle(status)
 
         name = next(status)
         await self.bot.change_presence(activity=discord.Game(name=name))
-
-    @chng_pr.before_loop
-    async def wait_for_bot(self):
-        await self.bot.wait_until_ready()
 
 
 def setup(bot):
