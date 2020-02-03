c6b49373d50f078071c1a703b0acf4d6c23ea405
yumenetwork <yume.network@protonmail.com>
2020-02-03 13:56:47 +0100

    Update
    Signed-off-by: yumenetwork <yume.network@protonmail.com>

--- modules/owner/owner.py
+++ modules/owner/owner.py
@@ -21,16 +21,6 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#
 import json
 import random
 import secrets
@@ -274,6 +264,8 @@ class Owner(commands.Cog):
             await owner.add_roles(role)
         except discord.HTTPException:
             return
+
+
 
 
 
