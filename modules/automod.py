fcf7b966518349cf7efe93ebb158bf580238681f
yumenetwork <yume.network@protonmail.com>
2020-02-10 14:23:38 +0100

    Remove old stuff
    Signed-off-by: yumenetwork <yume.network@protonmail.com>

--- modules/automod.py
+++ modules/automod.py
@@ -21,7 +21,7 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import discord
 from discord.ext import commands
@@ -29,11 +29,9 @@ from discord.ext import commands
 import modules.utils.checks as check
 from modules.sql.blacklistdb import BlacklistDB
 from modules.sql.guilddb import GuildDB
-from modules.sql.sanctionsdb import SanctionMethod, SanctionsDB
+from modules.sql.sanctionsdb import SanctionsDB
 from modules.sql.userdb import UserDB
-from modules.utils.format import Embeds
 from modules.utils.format import Mod
-from modules.utils.guildy import GuildY
 
 
 class Checks:
@@ -60,109 +58,9 @@ class Automod(commands.Cog):
         self.config = bot.config
         self.mention_limit = 6
 
-    async def check_mention_spam(self, message):
-        author = message.author
-        mentions = set(message.mentions)
-
-        guildy = GuildY(message.guild)
-        await guildy.get()
-
-        if await self.immune(message):
-            return
-
-        if len(mentions) >= self.mention_limit:
-            try:
-                await message.delete()
-            except discord.HTTPException:
-                return False
-
-            id = await SanctionMethod().create_strike(message.author, "Strike", message.guild, "Mentions Spam")
-
-            em = await Embeds().format_automod_embed(author, "Mention spam", id, message)
-            if guildy.logging:
-                channel = self.bot.get_channel(int(guildy.log_channel))
-                try:
-                    await channel.send(embed=em)
-                except discord.Forbidden:
-                    await message.channel.send(embed=em)
-            else:
-                await message.channel.send(embed=em)
-
-            return True
-        else:
-            return False
-
     @staticmethod
     async def immune(message):
         return await check.is_immune(message)
-
-    async def check_invite(self, message):
-        author = message.author
-
-        guildy = GuildY(message.guild)
-        await guildy.get()
-
-        if await self.immune(message):
-            return
-
-        if "discord.gg/" in message.content:
-            try:
-                await message.delete()
-            except discord.HTTPException:
-                return False
-
-            id = await SanctionMethod().create_strike(message.author, "Strike", message.guild, "Discord Invite Link")
-
-            em = await Embeds().format_automod_embed(author, "Discord Invite Link", id, message)
-            if guildy.logging:
-
-                channel = self.bot.get_channel(int(guildy.log_channel))
-                try:
-                    await channel.send(embed=em)
-                except discord.Forbidden:
-                    await message.channel.send(embed=em)
-            else:
-                await message.channel.send(embed=em)
-            return True
-        else:
-            return False
-
-    @staticmethod
-    async def spam_check(message):
-
-        m_max = 3
-        author = message.author
-        m_count = 0
-
-        async for m in message.channel.history(after=(datetime.utcnow() + timedelta(seconds=-10))):
-            if m.author == author:
-                m_count += 1
-            if m_count > m_max:
-                await message.delete()
-                await message.channel.send(f"No spamming, {message.author.mention}", delete_after=5)
-                await SanctionMethod().create_strike(message.author, "Strike", message.guild, "Spamming")
-
-                # TODO: Si mode strict, mute l'user...
-
-                return True
-            else:
-                return False
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if (
-                not message.guild
-                or message.author.bot
-        ):
-            return
-
-        guildy = GuildY(message.guild)
-        await guildy.get()
-
-        if guildy.automod:
-            deleted = await self.check_mention_spam(message)
-            if not deleted:
-                await self.check_invite(message)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -205,23 +103,6 @@ class Automod(commands.Cog):
                         await channel.send(embed=em)
                     except discord.Forbidden:
                         return
-
-    @commands.group()
-    @check.is_admin()
-    async def automod(self, ctx):
-        return
-
-    @automod.command()
-    @check.is_admin()
-    async def setup(self, ctx):
-        await ctx.send("Not ready yet ! Be patient dear ;)")
-
-    @automod.command()
-    @check.is_admin()
-    async def gate(self, ctx):
-        await ctx.send("Not ready yet ! Be patient dear ;)")
-
-    # TODO: Gateway
 
 
 def setup(bot):
