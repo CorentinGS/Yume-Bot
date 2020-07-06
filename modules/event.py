60de4d61d503427e97a3cd1743bd33ee74d9e4cf
yumenetwork <yume.network@protonmail.com>
2020-07-06 19:31:41 +0200

    fix events

--- modules/event.py
+++ modules/event.py
@@ -20,7 +20,7 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
-from random import random
+import random
 
 import discord
 from discord.ext import commands
