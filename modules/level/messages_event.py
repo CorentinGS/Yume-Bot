dc157138dc44a3f5a5a27202a39e4b22622ad4ef
Yume Network <yume.network@protonmail.com>
2020-08-31 10:34:43 +0200

    Change cooldown & remove ignored chans check

--- modules/level/messages_event.py
+++ modules/level/messages_event.py
@@ -13,7 +13,7 @@ class Level(commands.Cog):
         self.bot = bot
         self.config = bot.config
         self._cd = commands.CooldownMapping.from_cooldown(
-            1.0, 3.0, commands.BucketType.user)
+            1.0, 2.0, commands.BucketType.user)
 
     async def check(self, message: discord.Message):
         user = message.author
@@ -23,8 +23,8 @@ class Level(commands.Cog):
         if message.guild.id not in [488765635439099914, 631811291568144384, 618414922556112916, 740622438282428416]:
             return False
 
-        if RankingsDB.is_ignored_chan(message.channel.id):
-            return False
+        # if RankingsDB.is_ignored_chan(message.channel.id):
+         #   return False
 
         bucket = self._cd.get_bucket(message)
         retry_after = bucket.update_rate_limit()
